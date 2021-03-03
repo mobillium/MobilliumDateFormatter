e935305bf39933b763a9c2a267f74bad60df5e9a
Oğuzhan Karakuş <oguzhan.karakus@mobillium.com>
2021-03-03 17:57:19 +0300

    Add secons and milliseconds tests

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -50,4 +50,28 @@ class Tests: XCTestCase {
         XCTAssertNotNil(date)
     }
     
+    func testAddSecon() {
+        // Value
+        let date = Date(timeIntervalSince1970: 1614778571) // 2021-03-03 13:36:11
+        let date2 = Date(timeIntervalSince1970: 1614778576) // 2021-03-03 13:36:16
+        
+        // Create result Date
+        let resultDate = date.add(.second, count: 5)
+
+         // Check
+         XCTAssertEqual(date2, resultDate)
+    }
+    
+    func testAddMillisecond() {
+        // Value
+        let date = Date(timeIntervalSince1970: 1614778571) // 2021-03-03 13:36:11
+        let date2 = Date(timeIntervalSince1970: 1614778576) // 2021-03-03 13:36:16
+        
+        // Create result Date
+        let resultDate = date.add(.milisecond, count: 5000)
+
+         // Check
+         XCTAssertEqual(date2, resultDate)
+    }
+    
 }
